94a6d91deb71aeb0ce1d7d69ca416d3ab7c11f64
dabbler2 <117023230+dabbler2@users.noreply.github.com>
2023-10-24 16:49:02 +0900

    clicking poster shows new page with the trailer

---
+++ trailer.js
@@ -0,0 +1,15 @@
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZTFlZDhjM2I0NmM2ZTZiYjY4MjYzMzBiNzM0ZTlkMCIsInN1YiI6IjY1MmYyMWU4ZWE4NGM3MDEyZDcxYWQ1ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1sdyif5DYVbjFmwmxjyHKX8sMQM6MPPDHoleRZROXBU'
+  }
+};
+
+// 트레일러 긁어오기
+export async function scrape_trailer(id){
+	return await fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+		.then(res => res.json())
+	    .then(res => res.results[0].key)
+	    .catch(err => console.error(err));
+}

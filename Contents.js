e8866280ae1719c882d02a2a331d7c7b691f1636
dabbler2 <117023230+dabbler2@users.noreply.github.com>
2023-10-27 16:37:26 +0900

    css 공사

--- Contents.js
+++ Contents.js
@@ -7,7 +7,7 @@ const options = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZmMyZjNlYjc2OTRiYzdjNzljYjg3N2E3ZTRlNzlmMSIsInN1YiI6IjY1MmY5MGRlYT80dDGo7-1RtO-vBd97qa-2rhOt4dkmtfgifT9OZY8CEco'
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZTFlZDhjM2I0NmM2ZTZiYjY4MjYzMzBiNzM0ZTlkMCIsInN1YiI6IjY1MmYyMWU4ZWE4NGM3MDEyZDcxYWQ1ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1sdyif5DYVbjFmwmxjyHKX8sMQM6MPPDHoleRZROXBU'
   }
 };
 
@@ -19,7 +19,7 @@ fetch(`https://api.themoviedb.org/3/movie/${id}?language=en-US`, options)
     const $movieOver = document.querySelector(".movieContents");
     const $movieOverview1 = document.createElement("div");
     $movieOverview1.className = "movie-Cont";
-    $movieOverview1.innerHTML = `<p>${data.overview}</p>`;
+    $movieOverview1.innerHTML = `<p id="overview">${data.overview}</p>`;
     $movieOver.appendChild($movieOverview1);
   })
   .catch(err => console.error(err));
@@ -29,16 +29,18 @@ fetch(`https://api.themoviedb.org/3/movie/${id}?language=en-US`, options)
   .then((response) => response.json())
   .then((data) => {
     const $moviePerson = document.querySelector(".movieContents");
+	console.log(data.cast)
+	$moviePerson.innerHTML += `<p class="indent">출연진</p><hr>`
     const castData = data.cast;
+	const $movieCc = document.createElement("div");
+	$movieCc.id = "movie-castcrew";
     for(let i=0;i<Math.min(5,castData.length);++i){
-      const $movieCc = document.createElement("div");
-      $movieCc.className = "movie-castcrew";
-      $movieCc.innerHTML = `<div class = "movieC">
-        <img src="https://image.tmdb.org/t/p/w500${castData[i].profile_path}">
+      $movieCc.innerHTML += `<div class = "movieC">
+        <img class="photo" src="https://image.tmdb.org/t/p/w500${castData[i].profile_path}">
         <p>${castData[i].name}</p>
         </div>`;
-      $moviePerson.appendChild($movieCc);
     }
+	$moviePerson.appendChild($movieCc);
   })
   .catch(err => console.error(err));
 
--- detail.js
+++ detail.js
@@ -19,17 +19,17 @@ async function movieCast(id){
 }
 
 async function main2(){
-// 출연진 불러오기
+/* // 출연진 불러오기
 	let movieInfo = document.getElementById('movieInformation')
 	let cast = await movieCast(id)
 	for(let i=0;i<Math.min(5,cast.length);++i)
-		movieInfo.innerHTML += `<p>${cast[i].name}</p>`
+		movieInfo.innerHTML += `<p class="indent">${cast[i].name}</p>` */
 	
 	// 트레일러 불러오기
 	let trailerKey = await scrape_trailer(id)
 	let moviePreview = document.getElementById('moviePreview')
 	let trailerURL = 'https://www.youtube.com/embed/'+trailerKey
-	moviePreview.innerHTML = `<div><iframe src="${trailerURL}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>`
+	moviePreview.innerHTML += `<div><iframe id="trailer" src="${trailerURL}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>`
 }
 
 // 이미지 긁어오기
@@ -37,17 +37,17 @@ async function image(){
 	let imageKey = await scrape_image(id)
 	let movieInf = document.getElementById('movieInformation')
 	let imageURL = 'https://image.tmdb.org/t/p/original'+imageKey
-	movieInf.innerHTML = `<div><image src="${imageURL}" style="width:400px;height:400px;"></image></div>`
+	movieInf.innerHTML = `<div><image id="poster" src="${imageURL}"></image></div>`
 }
 
 // 정보 긁어오기
 async function information(){
 	let a = await scrape_information(id)
 	let inforDiv = document.getElementById('movieInformation')
-	inforDiv.innerHTML += `<div>
-		<p>영화제목 : ${a.title}</p>
+	inforDiv.innerHTML += `<div id="detail">
+		<p id="title">영화제목 : ${a.title}</p>
 		<p>개봉일 : ${a.release_date}</p>
-		<p>장르 : ${a.genres[0].name}</p>
+		<p>장르 : ${a.genres.map(a => a.name).join(', ')}</p>
 		<p>상영시간 : ${a.runtime}</p>
 		<p>평점 : ${a.vote_average}</p>
 	</div>`
